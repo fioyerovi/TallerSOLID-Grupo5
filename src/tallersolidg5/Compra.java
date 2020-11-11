e84b664a0693b7ea338df8d6b5d4227ee0414940
Fiorella Yerovi <fioreyerovi@gmail.com>
2020-11-11 12:43:23 -0500

    Creación del taller SOLID con el código inicial

---
+++ src/tallersolidg5/Compra.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tallersolidg5;
+
+import java.util.List;
+
+/**
+ *
+ * @author fioye
+ */
+public class Compra {
+    private Pago pago;
+    private PagoPayPal pagoPayPal;
+    private List articulos;
+    public Compra(Pago pago){
+        //iniciar
+    }
+    public void agregarArticulo(Articulo articulo){
+        //agregar articulo de compra
+    }
+    public void removerArticulo(Articulo articulo){
+        //remover articulo de compra
+    }
+}
---
+++ src/tallersolidg5/Pago.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tallersolidg5;
+
+/**
+ *
+ * @author fioye
+ */
+public class Pago {
+    //atributos
+    public void realizarCobro(double monto){
+        //cargar monto de compra
+    }
+    public void calcularImpuestosFactura(){
+        //calcula impuestos de compra
+    }
+    public void generarFactura(){
+        //genera nueva factura
+    }
+}
